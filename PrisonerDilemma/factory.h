227c2d174220ac73a0854cfa3b7b5295a7721d50
Tanya <70838088+tkubysheva@users.noreply.github.com>
2020-10-20 21:43:58 +0700

    Create factory.h

---
+++ PrisonerDilemma/factory.h
@@ -0,0 +1,40 @@
+#include "unit.h"
+#include <unordered_map>
+#include <vector>
+#include <iostream>
+
+
+template<class Product, class id>
+struct DefaultErrorPolicy{
+    static Product* IdNotFound(const id& name){
+        throw std::runtime_error("! ! ! WRONG ARGUMENTS ! ! !");
+    }
+};
+
+template<class Product, class id, typename Creator, class ErrorPolicy = DefaultErrorPolicy<Product, id>>
+class Factory {
+public:
+    Product* makeUnit(const id& name){
+        auto it = creators_.find(name);
+        if(it == creators_.end()){
+            return ErrorPolicy::IdNotFound(name);
+        }
+        std::pair<id, Creator> element = *it;
+        return element.second();
+    }
+    void addCreator(const id name, creator_t c){
+        creators_[name] = c;
+    }
+
+    static Factory*getInstance(){
+        static Factory f;
+        return &f;
+    }
+private:
+    Factory() = default;
+    ~Factory() = default;
+    Factory(const Factory&);
+    Factory& operator=(const Factory&);
+
+    std::unordered_map<id, Creator> creators_;
+};
