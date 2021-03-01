ec9bb73ea40e8d76a37762f64f8e4a907e68515d
Tanya <70838088+tkubysheva@users.noreply.github.com>
2021-03-01 11:05:50 +0700

    Update Reflection.java

--- Befunge/Reflection.java
+++ Befunge/Reflection.java
@@ -6,40 +6,8 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.Properties;
 
-public class Reflection {
-    /*
-    private static final HashMap<Character, String> commandName = new HashMap<>(){{
-        put('>', "commands.TurnRight");
-        put('v', "commands.TurnDown");
-        put('<', "commands.TurnLeft");
-        put('^', "commands.TurnUp");
-        put('?', "commands.TurnRandom");
-        put('+', "commands.Add");
-        put('-', "commands.Subtraction");
-        put('*', "commands.Multiplication");
-        put('/', "commands.Divide");
-        put('%', "commands.Modulo");
-        put('!', "commands.Not");
-        put('`', "commands.Greater");
-        put('_', "commands.HorizontalIf");
-        put('|', "commands.VerticalIf");
-        put('\"', "commands.StringMode");
-        put(':', "commands.Dup");
-        put('\\', "commands.Swap");
-        put('$', "commands.Pop");
-        put('.', "commands.OutputInt");
-        put(',', "commands.OutputChar");
-        put('#', "commands.Bridge");
-        put('g', "commands.Get");
-        put('p', "commands.Put");
-        put('&', "commands.InputInt");
-        put('~', "commands.InputCharacter");
-        put('@', "commands.End");
-        put('i', "commands.InStack");
-
-    }};
-
-     */
+public class Reflection { 
+    private static final HashMap<Character, Command> commandObject= new HashMap<>();
     private static final Properties commandName = new Properties();
     public static void Loader(){
         try {
@@ -59,7 +27,7 @@ public class Reflection {
             System.err.println(e);
         }
     }
-    private static final HashMap<Character, Command> commandObject= new HashMap<>();
+   
 
     private static Command InputClass(Character className) throws Exception{
         java.lang.Class<?> cl = null;
