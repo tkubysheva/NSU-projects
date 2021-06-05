0eb0a6e3b377536016337fad20190a4dc171fc8b
Tanya <70838088+tkubysheva@users.noreply.github.com>
2021-06-05 20:12:24 +0700

    Update Client.java

--- torrent/Client.java
+++ torrent/Client.java
@@ -1,37 +1,50 @@
-import java.io.*;
-import java.net.InetSocketAddress;
+import java.io.IOException;
 import java.net.Socket;
 import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
 
 
 public class Client {
-    private SocketChannel socketChannel;
+    //private SocketChannel socketChannel;
     private ByteBuffer buffer;
     private Socket clientSocket;
-    private BufferedReader in;
-    private BufferedWriter out;
+
 
     public Client() {
         try {
-            socketChannel = SocketChannel.open(new InetSocketAddress("localhost", 4000));
+            //socketChannel = SocketChannel.open(new InetSocketAddress("localhost", 4000));
             clientSocket = new Socket("localhost", 4000);
-            in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()));
-            buffer = ByteBuffer.allocate(256);
+            buffer = ByteBuffer.allocate(1024);
+            Handshake handshake = new Handshake("", "");
+            clientSocket.getOutputStream().write(handshake.getHandshake());
+            byte[] newHandshake = new byte[68];
+            clientSocket.getInputStream().read(newHandshake);
+            boolean flagConnection = true;
+            for (int i = 0; i < 68; ++i) {
+                if (newHandshake[i] != handshake.getHandshake()[i]) {
+                    flagConnection = false;
+                    break;
+                }
+            }
+            if (!flagConnection) {
+                clientSocket.close();
+            }
+            else{
+                sendMessage("Hello!");
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
+
         System.out.println("Client is ready!");
     }
 
     public void sendMessage(String msg) {
         buffer = ByteBuffer.wrap(msg.getBytes());
         try {
-            socketChannel.write(buffer);
+            clientSocket.getOutputStream().write(buffer.array());
             buffer.clear();
 
-            socketChannel.read(buffer);
+            clientSocket.getInputStream().read(buffer.array());
             System.out.println(new String(buffer.array()));
             buffer.clear();
 
@@ -43,12 +56,14 @@ public class Client {
 
     public static void main(String[] args) {
         Client client1 = new Client();
-        client1.sendMessage("Hello!");
+        client1.sendMessage("Hello!!");/*
         client1.sendMessage("My name is Client!");
         client1.sendMessage("exit");
         Client client2 = new Client();
         client2.sendMessage("I'm second");
         client2.sendMessage("I'm happy");
         client2.sendMessage("exit");
+
+         */
     }
 }
